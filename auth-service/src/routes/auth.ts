4192b2761dd457173bdc7e0b47854a381829721a
Theoneste-1 <theodufi.rw@gmail.com>
2025-09-08 15:41:43 +0200

    all done

--- auth-service/src/routes/auth.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import authController from '../cotrollers/authController.js';
-
-const router = Router();
-
-router.use('/', authController);
-
-export default router;
--- payroll-service/src/services/prisma.service.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient} from '@prisma/client';
-
-export const prismaService = new PrismaClient({
-  log: ['query', 'info', 'warn', 'error'],
-  errorFormat: 'pretty',
-});
